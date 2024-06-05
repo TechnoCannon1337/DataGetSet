c2cbc99e2d88535025a81bd272922d3bac280462
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2024-06-05 12:48:23 -0700

    Create get_github_h_index_metric_data.cpp

---
+++ GitHub/DataMetrics/HirschIndexMetric/Draft/ImplementationFiles/get_github_h_index_metric_data.cpp
@@ -0,0 +1,7 @@
+GitHubHIndexMetricCalculator::GetGitHubHIndexMetricData()
+{
+  ofstream organizationCSVFile("GitHubOrganizationCSVFile.csv");
+  ofstream userCSVFile("GitHubUserCSVFile.csv");
+  ofstream TopTenOrganizationCSVFile("TopTenGitHubOrganizationCSVFile.csv");
+  ofstream TopTenUserCSVFile("TopTenGitHubUserCSVFile.csv");
+}
