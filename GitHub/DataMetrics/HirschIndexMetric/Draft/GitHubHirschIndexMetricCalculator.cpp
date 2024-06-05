0e591b5ab6349c60070a3e28eb348cf6d40f02b6
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2024-06-05 00:29:09 -0700

    Update GitHubHirschIndexMetricCalculator.cpp

--- GitHub/DataMetrics/HirschIndexMetric/Draft/GitHubHirschIndexMetricCalculator.cpp
+++ GitHub/DataMetrics/HirschIndexMetric/Draft/GitHubHirschIndexMetricCalculator.cpp
@@ -5,21 +5,31 @@
 class GitHubHIndexMetricCalculator
 {
 private:
+  string list_all_user_repo_curl_=  "https://api.github.com/users/{user_name_}/repos?per_page=100&page={page_number_}";
+  string list_all_org_repo_curl_=  "https://api.github.com/orgs/{organization_name_}/repos?per_page=100&page={page_number_}";
+  string list_all_organizations_curl_=  "https://api.github.com/organizations?per_page=100&page={page_number_}";
+  string list_all_users_curl_=  "https://api.github.com/user";
+  string get_rate_limit_curl_=  "https://api.github.com/rate_limit";
+  int curl_header_ = "Accept: application/vnd.github+json", "Authorization: Bearer <YOUR-TOKEN>" , "X-GitHub-Api-Version: 2022-11-28";
+  int total_pages_;
+  int page_number_;
+  int response_status_code_;
   string user_name_;
+  string **user_name_and_id_array[1][2];
   string organization_name_;
+  string **organization_name_and_id_array[1][2];
   string repository_name_;
   string user_and_organization_url_;
   int rate_limit_;
   int rate_limit_reset_time_;
   int user_ID_;
-  int total_user_and_organization_count_;
+  int total_user_count_;
+  int total_organization_count_;
   int organization_ID_;
   int repository_ID_;
   int repository_forks_;
   int repository_stars_;
   int total_repository_count_;
-  int total_pages_;
-  int page_number_;
   bool repository_fork_status_;
   int github_h_index_result_;
   int **github_h_index_2D_metric_array;
@@ -44,7 +54,7 @@ public:
 };
 
 //Initialize & Define Constructor, Methods, and Variables.
-GitHubHIndexMetricCalculator::GitHubHIndexMetricConstructor(nullptr) : github_h_index_2D_metric_array(), total_repository_count_(0), current_size(0), github_total_user_metric_report_array(), total_user_and_organization_count_(0) {}
+GitHubHIndexMetricCalculator::GitHubHIndexMetricConstructor(nullptr) : github_h_index_2D_metric_array(), total_repository_count_(0), current_size(0), github_total_user_metric_report_array(), total_user_count_(0), total_organization_count_(0), user_name_and_id_array(), organization_name_and_id_array() {}
 
 DeleteOldArray(int count, int array)
 {
