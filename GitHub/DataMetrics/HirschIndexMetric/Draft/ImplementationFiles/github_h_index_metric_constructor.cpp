2cf8fc10cc3288484f72a289aed6f25de6d819e0
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2024-06-06 05:12:27 -0700

    Update github_h_index_metric_constructor.cpp

--- GitHub/DataMetrics/HirschIndexMetric/Draft/ImplementationFiles/github_h_index_metric_constructor.cpp
+++ GitHub/DataMetrics/HirschIndexMetric/Draft/ImplementationFiles/github_h_index_metric_constructor.cpp
@@ -1,13 +1,15 @@
-#include "../HeaderFiles/github_hirsch_index_metric_calculator.h"
+//#include "../HeaderFiles/github_hirsch_index_metric_calculator.h"
+#include "./github_hirsch_index_metric_calculator.h"
 GitHubHIndexMetricCalculator::GitHubHIndexMetricCalculator() : github_h_index_2D_metric_array(nullptr), total_repository_count_(0), current_size_(0), github_total_user_metric_report_array(nullptr), total_user_and_organization_count_(0), total_user_count_(0), total_organization_count_(0), user_array(nullptr), organization_array(nullptr) {}
 
-void DeleteOldArray(int count, int array)
+size_t DeleteOldArray(size_t count, auto array)
 {
   for (size_t i = 0; i < count; ++i)
   {
     delete[] array[i];
   }
   delete[] array;
+  return 0;
 }
 
 GitHubHIndexMetricCalculator::~GitHubHIndexMetricCalculator()
@@ -16,6 +18,4 @@ GitHubHIndexMetricCalculator::~GitHubHIndexMetricCalculator()
   DeleteOldArray(total_organization_count_, organization_array);
   DeleteOldArray(total_repository_count_, github_h_index_2D_metric_array);
   DeleteOldArray(total_user_and_organization_count_, github_total_user_metric_report_array);
-}
-
 }
