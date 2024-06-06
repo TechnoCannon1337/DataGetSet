ee3db27416834edb5acae093fb137ed5b8ee2c72
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2024-06-05 19:16:25 -0700

    Create main_test_request.cpp

---
+++ GitHub/DataMetrics/HirschIndexMetric/Draft/MainFiles/main_test_request.cpp
@@ -0,0 +1,10 @@
+#include "../HeaderFiles/github_hirsch_index_metric_calculator.h"
+
+int main()
+{
+  GitHubHIndexMetricCalculator testGitHubHIndexMetricCalculator;
+  testGitHubHIndexMetricCalculator.RequestGitHubAccounts();
+  std::cin.get();
+
+  return 0;
+}
