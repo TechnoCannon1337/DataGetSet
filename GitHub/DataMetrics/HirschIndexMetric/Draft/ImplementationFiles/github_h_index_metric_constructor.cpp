32a9ace9405fc6fb303c9092bff81ccadd441522
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2024-06-06 06:06:35 -0700

    Update github_h_index_metric_constructor.cpp

--- GitHub/DataMetrics/HirschIndexMetric/Draft/ImplementationFiles/github_h_index_metric_constructor.cpp
+++ GitHub/DataMetrics/HirschIndexMetric/Draft/ImplementationFiles/github_h_index_metric_constructor.cpp
@@ -1,21 +1,34 @@
 //#include "../HeaderFiles/github_hirsch_index_metric_calculator.h"
 #include "./github_hirsch_index_metric_calculator.h"
-GitHubHIndexMetricCalculator::GitHubHIndexMetricCalculator() : github_h_index_2D_metric_array(nullptr), total_repository_count_(0), current_size_(0), github_total_user_metric_report_array(nullptr), total_user_and_organization_count_(0), total_user_count_(0), total_organization_count_(0), user_array(nullptr), organization_array(nullptr) {}
 
-size_t DeleteOldArray(size_t count, auto array)
-{
-  for (size_t i = 0; i < count; ++i)
-  {
-    delete[] array[i];
-  }
-  delete[] array;
-  return 0;
+GitHubHIndexMetricCalculator::GitHubHIndexMetricCalculator()
+    : github_h_index_2D_metric_array(nullptr),
+      total_repository_count_(0),
+      current_size_(0),
+      github_total_user_metric_report_array(nullptr),
+      total_user_and_organization_count_(0),
+      total_user_count_(0),
+      total_organization_count_(0),
+      user_array(nullptr),
+      organization_array(nullptr) {}
+
+// Templated function to delete old arrays
+template <typename T>
+size_t DeleteOldArray(size_t count, T* array) {
+    for (size_t i = 0; i < count; ++i) {
+        delete[] array[i];
+    }
+    delete[] array;
+    return 0;
 }
 
-GitHubHIndexMetricCalculator::~GitHubHIndexMetricCalculator()
-{
-  DeleteOldArray(total_user_count_, user_array);
-  DeleteOldArray(total_organization_count_, organization_array);
-  DeleteOldArray(total_repository_count_, github_h_index_2D_metric_array);
-  DeleteOldArray(total_user_and_organization_count_, github_total_user_metric_report_array);
+GitHubHIndexMetricCalculator::~GitHubHIndexMetricCalculator() {
+    DeleteOldArray(total_user_count_, user_array);
+    DeleteOldArray(total_organization_count_, organization_array);
+    DeleteOldArray(total_repository_count_, github_h_index_2D_metric_array);
+    DeleteOldArray(total_user_and_organization_count_, github_total_user_metric_report_array);
 }
+
+// Explicit template instantiation for types used in the destructor
+template size_t DeleteOldArray<std::string>(size_t, std::string*);
+template size_t DeleteOldArray<int>(size_t, int*);
